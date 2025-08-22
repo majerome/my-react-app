b5977cefd544f3fb169970a5d10ff24a7928fbab
jelegrand <jelegrand@sqli.com>
2025-08-22 12:48:02 +0200

    Use a conditional expression to display information about the Pokemon if it is defined.

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,8 +1,25 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 function PokemonCard() {
-  return <figure>
-    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Bulbasaur" />
-    <figcaption>Bulbasaur</figcaption>
-  </figure>;
+    const pokemon = pokemonList[1];
+    
+    return <figure>
+        {
+        pokemon.imgSrc !== undefined ? 
+        <img src={pokemon.imgSrc} alt={pokemon.name} /> :
+        <p>???</p>
+        }
+    <figcaption>{pokemon.name}</figcaption>
+    </figure>;
 }
 
 export default PokemonCard;
