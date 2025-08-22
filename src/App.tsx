596a6c4c89dd737067d63ffd7c3ebfb25e506a07
jelegrand <jelegrand@sqli.com>
2025-08-22 19:03:08 +0200

    Apply css styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,24 +7,36 @@ const pokemonList = [
     name: "bulbasaur",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    color: "darkgreen",
+    backgroundColor: "lightgreen",
   },
   {
     name: "charmander",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    color: "firebrick",
+    backgroundColor: "lightsalmon",
   },
   {
     name: "squirtle",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    color: "navy",
+    backgroundColor: "lightblue",
   },
   {
     name: "pikachu",
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    color: "goldenrod",
+    backgroundColor: "lightyellow",
   },
   {
     name: "mew",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/151.png",
+    color: "orchid",
+    backgroundColor: "plum",
   },
 ];
 
@@ -46,7 +58,7 @@ function App() {
           </button>
         ))}
       </nav>
-      <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc} />
+      <PokemonCard name={pokemon.name} imgSrc={pokemon.imgSrc} color={pokemon.color} backgroundColor={pokemon.backgroundColor} />
     </div>
   );
 }
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,14 +1,23 @@
 interface Pokemon {
     name: string;
     imgSrc?: string;
+    color?: string;
+    backgroundColor?: string;
 }
 
 
-function PokemonCard({name, imgSrc}: Pokemon) {
-    return <figure>
+function PokemonCard({name, imgSrc, color, backgroundColor }: Pokemon) {
+    return <figure className="card" style={{
+        color: color,
+        backgroundColor: backgroundColor,
+    }}>
         {
         imgSrc !== undefined ? 
-        <img src={imgSrc} alt={name} /> :
+        <img 
+            src={imgSrc} 
+            alt={name} 
+            className="card-img"
+        /> :
         <p>???</p>
         }
     <figcaption>{name}</figcaption>
